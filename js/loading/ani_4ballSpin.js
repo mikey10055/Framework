dfa5f1a85edc77d15610475e58a2de02835fd57f
mikey10055 <mikey10055@gmail.com>
2015-09-08 21:53:52 +0100

    Inital Commit

---
+++ js/loading/ani_4ballSpin.js
@@ -0,0 +1,6 @@
+/* ani_4ballSpin
+  @authers - Michael Thomas
+*/
+var spinner = '<div class="ball ball-1"></div><div class="ball ball-2"></div><div class="ball ball-3"></div><div class="ball ball-4"></div>';
+$('.ani_4ballSpin').html(spinner);
+$('.ani_4ballSpinRandom').html(spinner);
---
+++ js/para.js
@@ -0,0 +1,20 @@
+/**
+* Paradox JS
+*     Version: 0.1
+*     Authers:
+*           Michael Thomas
+*
+*
+**/
+
+var Paradox = function(){
+
+  console.log('Paradox Js Initalized')
+};
+
+Paradox.prototype.hello = function() {
+  alert('hello');
+}
+
+
+paradox = new Paradox();
