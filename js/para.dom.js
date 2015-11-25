b1ee0818ae858807efa88bbd013167ca4e44b407
mikey10055 <mikey10055@gmail.com>
2015-11-25 01:04:06 +0000

    basic dom started, still wip

---
+++ js/para.dom.js
@@ -0,0 +1,41 @@
+/**
+* Paradox.dom JS
+*     Version: 0.0.1
+*     Authers:
+*           Michael Thomas
+*     notes:
+*           -Name might change
+*           -basic dom
+*
+**/
+
+paradox.dom = (function(){
+  var Dom = function (selector) {
+      var selector = document.querySelectorAll(selector);
+      this.length = selector.length;
+
+      for (var i = 0; i < this.length; i++) {
+        this[i] = selector[i];
+      }
+
+      return this;
+  };
+  return Dom;
+})();
+
+paradox.dom.prototype.html = function (string) {
+  for (var i = 0; i < this.length; i++) {
+    this[i].innerHTML = string;
+  }
+  return this;
+}
+paradox.dom.prototype.bg = function(color) {
+  for (var i = 0; i < this.length; i++) {
+    this[i].style.backgroundColor = color;
+  }
+  return this;
+}
+
+var $P = function (selector) {
+    return new paradox.dom(selector);
+};
